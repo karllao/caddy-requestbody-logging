94f6701d78cc2c39be3a48461ed947fcaa459e1d
karllao <644183866@qq.com>
2023-05-05 14:27:18 +0800

    v1.0.2

--- requestbody.go
+++ requestbody.go
@@ -1,57 +1,43 @@
 package requestbody
 
 import (
-    "bytes"
-    "context"
-    "encoding/json"
-    "net/http"
+	"bytes"
+	"io/ioutil"
+	"net/http"
 
-    "go.uber.org/zap"
+	"go.uber.org/zap"
     "go.uber.org/zap/zapcore"
-    "github.com/caddyserver/caddy/v2"
-    "github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
 )
 
-type Middleware struct {
-    Next    http.Handler
+func init() {
+	caddy.RegisterModule(Middleware{})
 }
 
-func (m Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+type Middleware struct{}
 
-    buffer := &bytes.Buffer{}
-    tee := io.TeeReader(r.Body, buffer)
-
-    var bodyJSON interface{}
-    if err := json.NewDecoder(tee).Decode(&bodyJSON); err != nil && err != io.EOF {
-        caddy.Log().Error("Error decoding request body", zap.Error(err))
-    }
-
-    // Restore the original request body before passing to the next handler.
-    r.Body = ioutil.NopCloser(bytes.NewReader(buffer.Bytes()))
-
-    logger := getLogger(r.Context())
-    logger.Info("Request Body", zap.Any("request", bodyJSON))
-
-    m.Next.ServeHTTP(w, r)
+func (Middleware) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "http.handlers.requestbody",
+		New: func() caddy.Module { return new(Middleware) },
+	}
 }
 
 func getLogger(ctx context.Context) *zap.Logger {
     return ctx.Value(caddy.LogContextKey).(*zap.Logger)
 }
 
-func New() *Middleware {
-    return &Middleware{}
-}
+func (m Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
 
-func (mw *Middleware) CaddyModule() caddy.ModuleInfo {
-    return caddy.ModuleInfo{
-        ID:  "http.handlers.requestbody",
-        New: func() caddy.Module { return new(Middleware) },
-    }
-}
+	r.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
 
-func (mw *Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
-    mw.Next = next
-    mw.ServeHTTP(w, r)
-    return nil
+    logger := getLogger(r.Context())
+    logger.Info("Request Body", zap.Any("request", string(bodyBytes)))
+	
+	return next.ServeHTTP(w, r)
 }
